77da3f2e001c1a69b70fbc6942479e03ee491d88
Sahil Singh <sahilsingh99433@gmail.com>
2025-05-23 12:23:54 +0530

    Update module.py
    comment the whole program

--- Chapter 1/module.py
+++ Chapter 1/module.py
@@ -1,13 +1,13 @@
-import pyjokes
+# import pyjokes
 
-# print("Printing Jokes...")
+# # print("Printing Jokhhhes...")
 
-# This prints a random joke
-joke = pyjokes.get_joke()
-print(joke)
+# # This prints a random joke
+# joke = pyjokes.get_joke()
+# print(joke)
 
 
-# so thanks
-# that was my program
-# another line
-# Yet another line
+# # so thanks
+# # that was my program
+# # another line
+# # Yet another line
